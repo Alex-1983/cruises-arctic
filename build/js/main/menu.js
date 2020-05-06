07fc1af175e56905f6f19581682e16c9c338a2db
Alex-1983 <1983-1111@mail.ru>
2020-05-06 14:59:23 +0500

    добавляет футер

--- build/js/main/menu.js
+++ build/js/main/menu.js
@@ -1,15 +1,12 @@
 'use strict';
 
-(function() {
+(function () {
   var mainHeader = document.querySelector('.main-header');
-  var burger = mainHeader.querySelector(".burger");
+  var burger = mainHeader.querySelector('.burger');
 
-
-  burger.addEventListener("click", function () {
-    mainHeader.classList.toggle("main-header--open");
-    burger.classList.toggle("burger--close");
+  burger.addEventListener('click', function () {
+    mainHeader.classList.toggle('main-header--open');
+    burger.classList.toggle('burger--close');
   });
-
-
 
 })();
